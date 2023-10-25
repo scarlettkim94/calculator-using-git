dff8a347c7ad83983760535eae7e25eb053a3b0c
tutor-kim <scarlett.kim94@gmail.com>
2023-10-25 13:10:22 +0900

    feature/modulo 브랜치 첫 커밋 - modulo() 작성

--- src/Main.java
+++ src/Main.java
@@ -47,6 +47,8 @@ public class Main {
     }
 
     private static void modulo(int a, int b) {
+        System.out.println(a + " % " + b + " = " + (a % b));
+
     }
 
     private static void divide(int a, int b) {
